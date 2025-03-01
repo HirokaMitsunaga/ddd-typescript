742b6898fc811ef901c94d7a3f9e473f7619a069
hiroka <mitsunagahrk87@gmail.com>
2025-03-01 11:19:04 +0900

    初期設定

---
+++ src/sayHello.test.ts
@@ -0,0 +1,5 @@
+import { sayHello } from "sayHello";
+
+test("sayHello test", () => {
+  expect(sayHello("aaaa")).toBe("aaaa");
+});
---
+++ src/sayHello.ts
@@ -0,0 +1,5 @@
+export function sayHello(name: string): string {
+  return name;
+}
+
+sayHello("ore");
